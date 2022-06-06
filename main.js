fb31a09d383c9fdbc403dbd32d0f16e089dd0541
PCpastre <paulo.pastre_juninho@hotmail.com>
2022-06-06 17:39:09 -0300

    1.0.0.1

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 4002;
+const port = process.env.PORT || 80;
 
 const request = require('request');
 
